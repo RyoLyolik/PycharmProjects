8c69edc4ba31ddc4fa1f03ca12f1810605f37191
bicho <no>
2018-12-18 22:33:58 +0300

    Done

--- IndiGame/main_win.py
+++ IndiGame/main_win.py
@@ -1,6 +1,3 @@
-# TRAINING
-
-
 import pygame
 import random
 import math
@@ -12,6 +9,7 @@ class Window:
         size = w, h = 720, 480
         screen = pygame.display.set_mode(size)
         self.player = Player()
+        self.enemy = Enemy()
         pygame.init()
 
         self.screen_update()
@@ -20,11 +18,16 @@ class Window:
         while pygame.event.wait().type != pygame.QUIT:
             self.key_events()
             self.player.draw_player()
+            self.enemy.draw_enemy()
+            self.colliding(self.player.player, self.enemy.player)
             pygame.display.flip()
             screen.fill((0,0,0))
 
+    def colliding(self, ob1, ob2):
+        if ob1.colliderect(ob2):
+            screen.fill(pygame.Color('red'), ob1.clip(ob2))
+
     def key_events(self):
-        # print(pygame.key.get_pressed())
         if pygame.key.get_pressed()[pygame.K_LEFT]:
             self.move(self.player.player, [-self.player.speed,0])
 
@@ -40,7 +43,8 @@ class Window:
     def move(self, ob, speed):
         # ob = ob.move(speed) # DONT WORK, WHY ??
         self.player.player = self.player.player.move(speed)
-        print(self.player.player.left)
+
+
 class Player:
     def __init__(self):
         global screen
@@ -49,7 +53,8 @@ class Player:
         self.speed = 2
         self.pos_x = 190
         self.pos_y = 60
-        self.player = pygame.draw.rect(screen, (200, 100, 150), (190, 60, self.player_size, self.player_size), 0)
+        self.player = pygame.draw.rect(screen, (200, 100, 150),
+                                       (self.pos_x, self.pos_y, self.player_size, self.player_size), 0)
         self.draw_player()
 
 
@@ -57,7 +62,16 @@ class Player:
         self.player = pygame.draw.rect(screen, (200, 100, 150), (self.player.left, self.player.top, self.player_size, self.player_size), 0)
 
 
+class Enemy(Player):
+    def __init__(self):
+        super().__init__()
+        self.pos_x = 400
+        self.pos_y = 150
+        self.player = pygame.draw.rect(screen, (200, 100, 150),
+                                       (self.pos_x, self.pos_y, self.player_size, self.player_size), 0)
 
+    def draw_enemy(self):
+        super().draw_player()
 
 
 if __name__ == '__main__':
